b749827ad1730990be610a020def9ad7b8ee4e3f
Jun Hyoung Lee <junhyoung@sphereidentity.com>
2021-02-02 20:56:56 +0900

    Destoryable interface added to safely delete data

---
+++ ratchet/src/main/java/app/junhyounglee/ratchet/core/Destroyable.kt
@@ -0,0 +1,5 @@
+package app.junhyounglee.ratchet.core
+
+interface Destroyable {
+  fun destroy()
+}
--- ratchet/src/main/java/app/junhyounglee/ratchet/core/KeyPair.kt
+++ ratchet/src/main/java/app/junhyounglee/ratchet/core/KeyPair.kt
@@ -1,6 +1,11 @@
 package app.junhyounglee.ratchet.core
 
-class KeyPair constructor(val publicKey: ByteArray, val secretKey: ByteArray) {
+class KeyPair constructor(val publicKey: ByteArray, val secretKey: ByteArray): Destroyable {
   operator fun component1(): ByteArray = publicKey
   operator fun component2(): ByteArray = secretKey
+
+  override fun destroy() {
+    publicKey.fill(0)
+    secretKey.fill(0)
+  }
 }
